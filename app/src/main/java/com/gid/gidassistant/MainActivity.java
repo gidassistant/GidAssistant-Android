beaf23f18307c3785ccd2a15d8a4600f92bff09c
yuriy070920 <surzhickov.yura@yandex.ua>
2020-07-27 15:12:59 +0300

    Create onClick method2

--- app/src/main/java/com/gid/gidassistant/MainActivity.java
+++ app/src/main/java/com/gid/gidassistant/MainActivity.java
@@ -3,6 +3,7 @@ package com.gid.gidassistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void setView(View view) {
+
+    }
+
 }
