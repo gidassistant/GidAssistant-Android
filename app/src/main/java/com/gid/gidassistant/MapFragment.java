916eb0483fbfd7786a80e6a891ddd13148a0f059
shuvalovdanil <shuvalovdanilWork@gmail.com>
2020-08-01 19:34:45 +0300

    te

--- app/src/main/java/com/gid/gidassistant/MapFragment.java
+++ app/src/main/java/com/gid/gidassistant/MapFragment.java
@@ -109,7 +109,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         mMap = googleMap;
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
