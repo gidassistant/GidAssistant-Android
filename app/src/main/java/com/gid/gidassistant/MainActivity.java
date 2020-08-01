d75f98b79980123b885457cb938d9ea683d725e1
yuriy070920 <surzhickov.yura@yandex.ua>
2020-08-01 16:00:17 +0300

    Added interests bottom slide up sheet

--- app/src/main/java/com/gid/gidassistant/MainActivity.java
+++ app/src/main/java/com/gid/gidassistant/MainActivity.java
@@ -1,32 +1,21 @@
 package com.gid.gidassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
-import android.app.FragmentTransaction;
 import android.os.Bundle;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.view.SurfaceControl;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.gid.gidassistant.dialogs.InterestsBottomDialog;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 23;
-    private GoogleMap mMap;
-    Button btn;
+    Button interestsButton;
+    LinearLayout linearLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +31,4 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.map_fragment, mapTab)
                 .commit();
     }
-
-
 }
